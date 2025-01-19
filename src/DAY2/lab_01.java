993e43b26bc23e6c1b6c2f1bd95ff6a4cae78ae3
Vishnu <vishnu01thapa02@gmail.com>
2025-01-19 08:57:56 +0530

    day 2

---
+++ src/DAY2/lab_01.java
@@ -0,0 +1,17 @@
+package DAY2;
+
+public class lab_01 {
+    public static void main(String[] args) {
+       int num = 9;
+        System.out.println(num);
+        System.out.println(num*2);
+        System.out.println(num*3);
+        System.out.println(num*4);
+        System.out.println(num*5);
+        System.out.println(num*6);
+        System.out.println(num*7);
+        System.out.println(num*8);
+        System.out.println(num*9);
+        System.out.println(num*10);
+    }
+}
